f380f40458c20de3e1878863f81dbee855301eb4
KethanMVS02 <144221283+KethanMVS02@users.noreply.github.com>
2023-10-18 12:57:45 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+function Header() {
+  return (
+    <header className="header">
+      <h1>MARADANA VENKATA SIVA KETHAN</h1>
+      <div className="tile">
+        <h3>Front End Developer</h3>
+      </div>
+    </header>
+  );
+}
+export default Header;
